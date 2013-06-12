4349e4255d5d770321d2e7ef65fa29607c9d3abc
Ian Nelson <iannelson1@gmail.com>
2013-06-12 18:29:28 -0400

    Updated Filter jsdoc.

--- js/processors/query/Filter.js
+++ js/processors/query/Filter.js
@@ -1,8 +1,26 @@
+/**
+ * Filter constructor.
+ * 
+ * @class Filter
+ * @module Processors
+ * @constructor
+ */
+
 var Filter = function() {}
 
 Filter.prototype = {
 	debugging: false
 };
+
+/**
+ * Default execution method for processors.
+ * 
+ * Filters results from MongoDB based on assigned columns and expressions.
+ * 
+ * @method run
+ * @param {Array} queryParameters List of relevant query parameters from the RESTful request
+ * @return {Object} Object describing MongoDB operation and parameters for the method
+ */
 
 Filter.prototype.run = function (queryParameters) {
 	var filterObj = {}
@@ -12,9 +30,23 @@ Filter.prototype.run = function (queryParameters) {
 	};
 };
 
+/**
+ * RESTful parameter fields relevant to the processor.
+ * 
+ * @method getQueryFields
+ * return {Array} List of relevant parameters
+ */
+
 Filter.prototype.getQueryFields = function () {
 	return ['filter'];
 };
+
+/**
+ * RESTful parameter field descriptions.
+ * 
+ * @method queryFieldParameters
+ * @return {Array} List of relevant field descriptions for the processor
+ */
 
 Filter.prototype.queryFieldParameters = function () {
 	return [{
